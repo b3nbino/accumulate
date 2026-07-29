231d49a3dcd059c0d6d00d4bba22bcd35b093fed
Benjamin Stevens <B3nbinoh@gmail.com>
2026-07-29 16:46:13 -0600

    Installed morgan middleware for request logging.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,10 @@
 import express, { type Express, type Request, type Response } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
