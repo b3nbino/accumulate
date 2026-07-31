b8d884b0f9283c4ff99f3f04991b04bdc797799e
Benjamin Stevens <B3nbinoh@gmail.com>
2026-07-30 18:34:39 -0600

    Renamed types file. Added helper and types directories. Creates body validation function for new entries. Added some functionality to POST /entries.

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,16 +4,17 @@
 import express, { type Express, type Request, type Response } from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { isValidNewEntry } from "./helpers/isValidNewEntry.js";
 
 // Types
-import type { Entry } from "./types.js";
+import type { Entry, NewEntry } from "./types/entries.js";
 
 const app: Express = express();
 const PORT = 3000;
 const ENTRIES: Entry[] = [
   {
     id: 1,
-    movie_id: 1,
+    db_id: 1,
     title: "Baby Driver",
     release_date: "March 11th, 2017",
     start_date: "July 7th, 2026",
@@ -42,9 +43,38 @@ app.get("/entries", (req: Request, res: Response) => {
 
 app.post("/entries", (req: Request, res: Response) => {
   // Validate request bodies, then add them to entries
-  let body = req.body;
-  console.log(body);
-  res.send("Recieved a request!");
+  let body: NewEntry = req.body;
+
+  if (isValidNewEntry(body)) {
+    let id = Math.floor(Math.random() * 1000) + 1;
+
+    // Extract the properties we want, which are validated, extra fields could be a security flaw
+    let entry: Entry = {
+      id,
+      db_id: body.db_id,
+      title: body.title,
+      release_date: body.release_date,
+      start_date: body?.start_date,
+      finish_date: body?.finish_date,
+      status: body.status,
+      progress: body?.progress,
+      user_rating: body?.user_rating,
+      review: body?.review,
+      liked: body.liked,
+      tags: body?.tags,
+    };
+
+    // Add to entries
+    ENTRIES.push(entry);
+
+    res.json(ENTRIES);
+  } else {
+    console.log("400: Bad request");
+
+    res.statusCode = 400;
+    res.send("There was a problem with your request.");
+    return;
+  }
 });
 
 // FIX ME: Add global error handler
