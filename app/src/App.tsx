90e3f445cc991f416a8e7b64e1a05214fb81e068
Benjamin Stevens <B3nbinoh@gmail.com>
2026-08-06 15:41:47 -0600

    Updated data model of example data. Updated diary mockup.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,56 +2,68 @@ function App() {
   const ENTRIES = [
     {
       id: 1,
-      db_id: 1,
+      media_id: 1,
+      source: "",
       title: "Baby Driver",
-      release_date: "March 11th, 2017",
-      start_date: "July 7th, 2026",
-      finish_date: "July 7th, 2026",
+      release_date: new Date("March 11 2017"),
+      start_date: new Date("July 7 2026"),
+      finish_date: new Date("July 7 2026"),
+      last_edited_date: new Date("August 8 2026"),
       status: "completed",
-      progress: "100%",
+      progress: 115,
+      total_length: 115,
+      progress_type: "minutes",
       user_rating: 10,
       review: "",
       liked: true,
-      tags: ["soundtrack", "driving"],
     },
     {
       id: 2,
-      db_id: 2,
+      media_id: 2,
+      source: "",
       title: "Neuromancer",
-      release_date: "March 11th, 2017",
-      start_date: "July 7th, 2026",
-      status: "completed",
-      progress: "5%",
+      release_date: new Date("March 11 2017"),
+      start_date: new Date("July 7 2026"),
+      last_edited_date: new Date("August 8 2026"),
+      status: "in-progress",
+      progress: 5,
+      total_length: 24,
+      progress_type: "chapters",
       liked: false,
-      tags: ["cyberpunk", "gritty"],
     },
     {
       id: 3,
-      db_id: 23,
+      media_id: 23,
+      source: "",
       title: "Minecraft",
-      release_date: "March 11th, 2017",
-      start_date: "July 7th, 2016",
-      finish_date: "February 6th, 2026",
+      release_date: new Date("March 11 2017"),
+      start_date: new Date("July 7 2016"),
+      finish_date: new Date("February 6 2026"),
+      last_edited_date: new Date("July 10 2026"),
       status: "completed",
-      progress: "100%",
-      user_rating: 10,
+      progress: 100,
+      total_length: 100,
+      progress_type: "percent",
+      user_rating: 9,
       review: "I Love Minecraft",
       liked: true,
-      tags: ["soundtrack", "sandbox", "nostalgic"],
     },
     {
       id: 4,
-      db_id: 14,
-      title: "Squid Game",
-      release_date: "September 12th, 2011",
-      start_date: "July 7th, 2026",
-      finish_date: "August 25th, 2026",
-      status: "completed",
-      progress: "100%",
-      user_rating: 9,
+      media_id: 14,
+      source: "",
+      title: "Squid Game S1",
+      release_date: new Date("September 12 2011"),
+      start_date: new Date("July 7 2026"),
+      finish_date: new Date("August 25 2026"),
+      last_edited_date: new Date("June 31 2026"),
+      status: "dropped",
+      progress: 2,
+      total_length: 9,
+      progress_type: "episodes",
+      user_rating: 1,
       review: "",
       liked: true,
-      tags: ["korean", "death games"],
     },
   ];
 
@@ -62,7 +74,7 @@ function App() {
           {/* Eventually we'll need to add the thumb URL. Once we connect the API. Make sure to change finish date to last edited date */}
           <ul id="entries">
             <li className="entry">
-              <p>{ENTRIES[0].finish_date}</p>
+              <p>{ENTRIES[0].last_edited_date.toDateString()}</p>
               <p>{ENTRIES[0].title}</p>
               <p>{ENTRIES[0].status}</p>
               <p>{ENTRIES[0].progress}</p>
@@ -70,7 +82,7 @@ function App() {
               <p>{ENTRIES[0].liked}</p>
             </li>
             <li className="entry">
-              <p>{ENTRIES[1].finish_date}</p>
+              <p>{ENTRIES[1].last_edited_date.toDateString()}</p>
               <p>{ENTRIES[1].title}</p>
               <p>{ENTRIES[1].status}</p>
               <p>{ENTRIES[1].progress}</p>
