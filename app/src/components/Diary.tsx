414385d22a56f5e005541eb15dcc18b3f915f91f
Benjamin Stevens <B3nbinoh@gmail.com>
2026-08-14 17:28:22 -0600

    Added state and effect that get entry from the server, replacing usage of the example data on the frontend.

--- app/src/components/Diary.tsx
+++ app/src/components/Diary.tsx
@@ -1,16 +1,34 @@
-import type { ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import type { EntryType } from "../types/EntryType";
-import { ENTRIES } from "../assets/data";
 import Entry from "./Entry";
 
 export default function Diary(): ReactNode {
+  const [entries, setEntries] = useState<EntryType[]>(() => {
+    return [];
+  });
+
+  useEffect(() => {
+    (async () => {
+      const req = await fetch("/entries");
+      const json = await req.json();
+
+      json.forEach((entry: EntryType) => {
+        if ("last_edited_date" in entry) {
+          entry.last_edited_date = new Date(entry.last_edited_date);
+        }
+      });
+
+      setEntries(json);
+    })();
+  }, []);
+
   function listEntries(allEntrires: EntryType[]): ReactNode {
     const entryList = allEntrires.map((entry) => {
-      return <Entry {...entry}></Entry>;
+      return <Entry key={entry.id} {...entry}></Entry>;
     });
 
     return <ul id="entries">{...entryList}</ul>;
   }
 
-  return <section id="diary">{listEntries(ENTRIES)}</section>;
+  return <section id="diary">{listEntries(entries)}</section>;
 }
